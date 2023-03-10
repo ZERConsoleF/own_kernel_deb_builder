67cc46d89ceee75dea841f5b67494b7bc5db8f2d
ZERConsoleF <2875967152@qq.com>
2023-03-10 23:35:39 +0800

    UPDATE work action

--- build_action.sh
+++ build_action.sh
@@ -9,6 +9,7 @@ sed -i "/deb-src/s/# //g" /etc/apt/sources.list
 # install dep
 apt update
 apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev zsh
+apt install -y git dwarves build-essential fakeroot bc kmod cpio libncurses5-dev libgtk2.0-dev libglib2.0-dev libglade2-dev libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev dpkg-dev autoconf libdw-dev cmake zstd
 apt build-dep -y linux
 
 # change dir to workplace
@@ -23,7 +24,7 @@ cd linux-"$VERSION" || exit
 cp ../config .config
 
 # disable DEBUG_INFO to speedup build
-scripts/config --disable DEBUG_INFO
+#scripts/config --disable DEBUG_INFO
 
 # extra build argument
 export EXTRA_ARGS=""
