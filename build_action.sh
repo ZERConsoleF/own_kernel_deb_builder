4695e6a1c87f7145b141be06f5e7017ca52b2651
ZERConsoleF <2875967152@qq.com>
2023-03-12 21:35:44 +0800

    UPDATE builder produce config - 5

--- build_action.sh
+++ build_action.sh
@@ -20,8 +20,7 @@ cd "${GITHUB_WORKSPACE}" || exit
 
 # download kernel source
 wget http://www.kernel.org/pub/linux/kernel/$VERCODE/linux-"$VERSION".tar.xz
-mkdir -p linux-"$VERSION"
-tar -xvf linux-"$VERSION".tar.xz -C linux-"$VERSION"/ >/dev/null
+tar -xf linux-"$VERSION".tar.xz
 cd linux-"$VERSION" || exit
 
 # copy config file
